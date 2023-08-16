a66e2585f89f31eb5e08ad8677e394da08096c5a
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-08-16 14:55:44 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - A program that computes addittion
+ * @a: the number to be computed
+ * @b: the number to be computed
+ * Return: 0 (Success)
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (0);
+}
