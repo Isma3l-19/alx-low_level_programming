d89365e280078b73d42991c65c4b0c11c040727c
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-08-21 15:05:31 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*Declaring the _putchar function*/
+int _putchar(char c);
+
+/**
+ * puts2 - Prints characters of strings
+ * @str: the string
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] != '\0')
+		{
+			_putchar('\n');
+		}
+		if (count % 2 == 0)
+		{
+			_putchar(str[count]);
+			count++;
+		}
+	}
+}
