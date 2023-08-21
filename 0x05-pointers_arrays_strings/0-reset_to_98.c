f165dae192267edab2d56c4b80f5fed2402bd5d0
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-08-21 12:52:48 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updating the value of a pointer n to 98
+ * @n: the value to be updated
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This Project about c pointers and arrays 
+0x05. C - Pointers, arrays and strings
