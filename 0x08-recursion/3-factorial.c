4786b2f5f87414d2425d0899d06d0e9431097f89
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-08-30 14:55:08 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,9 +6,16 @@
  */
 int factorial(int n)
 {
+	int i = n;
+
 	if (n < 1)
 	{
 		return (-1);
 	}
-	return (n * factorial(n - 1));
+	else if (n >= 0 && n <= 1)
+	{
+		return (1);
+	}
+	i = n * factorial(n - 1);
+	return (i);
 }
