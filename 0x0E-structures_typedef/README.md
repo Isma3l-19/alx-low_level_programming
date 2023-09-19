6e52ed423b348b0cad00aecabebb007251d5efec
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-09-19 03:49:59 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+0x0E-structures_typedef - This project is all about making and using structres and how to use the typedef function
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef dog
+#define dog
+/**
+ * dog - it is a structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
