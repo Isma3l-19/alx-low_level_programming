3db03a9a9dc17cb4a67e2d0ceb6bd91897fdb05a
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-10-02 07:58:25 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * read_textfile- a function that reads a text file and prints it to POSIX
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *       : 0 when function fails or filename = NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *ptr;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == NULL)
+	{
+		return (0);
+	}
+	ptr = malloc(sizeof(char) * letters);
+	t = read(fd, ptr, letters);
+	w = write(STDOUT_FILENO, ptr, t);
+	free(ptr);
+	close(fd);
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+0x15-file_io - This project is all about file creation and manipulation
